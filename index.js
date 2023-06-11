62e2a411c491a87922a28f61f5932ef0ea2c60da
Simon Njuguna Wanjira <snwnjuguna97@gmail.com>
2023-06-11 19:12:36 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+const arrStrNm=["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(arrStrNm, eventname) {
+    const nArr = [];
+    for (let i = 0; i < arrStrNm.length; i++) {
+        const message = (`Thank you, ${arrStrNm[i]}, for the wonderful ${eventname} gift!`) 
+        nArr.push(message);         
+    }
+    console.log(nArr);
+    return nArr;   
+}
+
+writeCards(arrStrNm, "surprise");
+
+function countDown(a){
+    while (a>=0){
+        console.log(a);
+        a--;
+    }
+}
+countDown(12);
